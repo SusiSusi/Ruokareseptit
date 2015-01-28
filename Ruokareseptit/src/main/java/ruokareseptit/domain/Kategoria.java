d6fe1ea3e963d786e364b47be27662268db9789e
Suvi <suvi.sipila@cs.helsinki.fi>
2015-01-28 14:27:39 +0200

    Testiluokka

--- Ruokareseptit/src/main/java/ruokareseptit/domain/Kategoria.java
+++ Ruokareseptit/src/main/java/ruokareseptit/domain/Kategoria.java
@@ -5,19 +5,19 @@ import java.util.*;
 import ruokareseptit.StringUtils;
 
 public class Kategoria {
-
+    
     private String nimi;
     private List<Resepti> reseptit;
-
+    
     public Kategoria(String nimi) {
         this.nimi = nimi;
         this.reseptit = new ArrayList<Resepti>();
     }
-
+    
     public String getKategorianNimi() {
         return this.nimi;
     }
-
+    
     public void lisaaReseptiKategoriaan(Resepti resepti) {
         this.reseptit.add(resepti);
     }
@@ -25,26 +25,27 @@ public class Kategoria {
     public int reseptienMaaraKategoriassa() {
         return this.reseptit.size();
     }
-
-    public void tulostaKaikkiReseptit() {
-        if (this.reseptit.isEmpty()) {
-            System.out.println("Kategoriaan " + this.nimi + " ei ole talletettu reseptejä.");
-        }
-        for (Resepti resepti : this.reseptit) {
-            System.out.println(resepti);
-        }
-    }
-
-    public void tulostaReseptienNimet() {
-        if (this.reseptit.isEmpty()) {
-            System.out.println("Kategoriaan " + this.nimi + " ei ole talletettu reseptejä.");
-        } else {
+    
+    public List tulostaKaikkiReseptit() {
+        List<Resepti> palautettavaLista = new ArrayList<Resepti>();
+        if (!this.reseptit.isEmpty()) {
             for (Resepti resepti : this.reseptit) {
-                System.out.println(resepti.getNimi());
+                palautettavaLista.add(resepti);
             }
         }
+        return palautettavaLista;
     }
-
+    
+    public List tulostaReseptienNimet() {
+        List<String> palautettavaLista = new ArrayList<String>();
+        if (!this.reseptit.isEmpty()) {
+            for (Resepti resepti : this.reseptit) {
+                palautettavaLista.add(resepti.getNimi());
+            }
+        }
+        return palautettavaLista;
+    }
+    
     public void tulostaResepti(String nimi) {
         int apu = 0;
         if (!this.reseptit.isEmpty()) {
@@ -54,7 +55,8 @@ public class Kategoria {
                     apu = 1;
                 }
             }
-        } if (this.reseptit.isEmpty() || apu == 0) {
+        }
+        if (this.reseptit.isEmpty() || apu == 0) {
             System.out.println("Valitsemaasi reseptiä ei löytynyt.");
         }
     }
--- Ruokareseptit/src/test/java/ruokareseptit/domain/KategoriaTest.java
+++ Ruokareseptit/src/test/java/ruokareseptit/domain/KategoriaTest.java
@@ -1,12 +1,12 @@
-
 package ruokareseptit.domain;
 
+import java.util.*;
 import org.junit.After;
 import org.junit.AfterClass;
+import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 public class KategoriaTest {
     
@@ -29,16 +29,36 @@ public class KategoriaTest {
     }
     
     @Test
-    public void tulostaaReseptienNimet() {
+    public void tulostaaReseptienNimetOikein() {
         keitot.lisaaReseptiKategoriaan(new Resepti("Maksalaatikko"));
         keitot.lisaaReseptiKategoriaan(new Resepti("Risotto"));
         keitot.lisaaReseptiKategoriaan(new Resepti("Kastike"));
         keitot.lisaaReseptiKategoriaan(new Resepti("Jauheliha"));
-        assertEquals(4, keitot.reseptienMaaraKategoriassa());
+        List<String> lista = new ArrayList<String>();
+        lista.add("Maksalaatikko");
+        lista.add("Risotto");
+        lista.add("Kastike");
+        lista.add("Jauheliha");        
+        assertEquals(lista, keitot.tulostaReseptienNimet());
     }
     
     @Test
     public void tulostaaReseptienMaaranNollaTapauksessa() {
         assertEquals(0, keitot.reseptienMaaraKategoriassa());
     }
+    
+    @Test
+    public void tulostaaKaikkiReseptitOikein() {
+        Resepti maksa = new Resepti("Maksalaatikko");
+        Resepti risotto = new Resepti("Risotto");
+        keitot.lisaaReseptiKategoriaan(maksa);
+        keitot.lisaaReseptiKategoriaan(risotto);
+        keitot.lisaaReseptiKategoriaan(kalakeitto);
+        List<Resepti> lista = new ArrayList<Resepti>();
+        lista.add(maksa);
+        lista.add(risotto);
+        lista.add(kalakeitto);
+        assertEquals(lista, keitot.tulostaKaikkiReseptit());
+    }
+    
 }
