b626b6849561d9fea8b8181f65672ccc91cd5005
Suvi <suvi.sipila@cs.helsinki.fi>
2015-02-02 15:36:30 +0200

    kayttoliittymaa

---
+++ Ruokareseptit/src/main/java/ruokareseptit/Main.java
@@ -0,0 +1,14 @@
+package ruokareseptit;
+
+import javax.swing.SwingUtilities;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Kayttoliittyma kayttoliittyma = new Kayttoliittyma();
+        SwingUtilities.invokeLater(kayttoliittyma);
+
+    }
+
+}
